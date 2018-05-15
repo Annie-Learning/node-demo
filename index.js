e49e1514e46714a03a64d4b0221e737a22fa276e
Annie <a10423006@yuntech.org.tw>
2018-05-15 20:38:57 +0800

    add helmet & serve-static

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const Express = require('express');
+const helmet = require('helmet');
+const serveStatic = require('serve-static');
 
 const app = Express();
+
+app.use(helmet());
+app.use(serveStatic('public'));
 
 app.get('/', function(req, res) {
     console.log('Hello');
     res.send('Hello World');
+})
+
+app.get('/home', function(req, res) {
+    console.log('Hello');
+    res.send('Hello Home');
+})
+
+app.get('user/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send('user id: ' + req.params.id);
 })
 
 app.listen(3000, function() {
